ea8f0dfc6b6d2f024fbd1fccbb9b4b70083d4472
ikWare <somto.ike.dev@gmail.com>
2026-02-18 22:26:23 +0100

    feat: Update random number generation to produce integers within a specified range for all buttons.

--- script.js
+++ script.js
@@ -15,25 +15,23 @@ let initial = 'Click the buttons below to generate a random number from 1 - 100'
 document.getElementById("label").textContent = initial
 
 document.getElementById("btn1").onclick = function(){
-    btn1 = Math.random();
+    btn1 = Math.floor(Math.random() * btn1Max) + btn1Min
     document.getElementById("label").textContent = btn1
     console.log('you tried randomizing 1 - 25')
 }
 document.getElementById("btn2").onclick = function(){
-    btn2 = Math.random();
+    let btn2 = Math.floor(Math.random() * (btn2Max - btn2Min + 1)) + btn2Min;
     document.getElementById("label").textContent = btn2
     console.log('you tried randomizing 26 - 50')
 }
 document.getElementById("btn3").onclick = function(){
-    btn3 = Math.random();
+    btn3 = Math.floor(Math.random() * (btn3Max - btn3Min + 1)) + btn3Min;
     document.getElementById("label").textContent = btn3
     console.log('you tried randomizing 51 - 75')
 }
-document.getElementById("btn4").onclick = function getBtn4Random(btn4Min, btn4Max){
-    let minCeiled = Math.ceil(btn4Min);
-    let maxFloored = Math.floor(btn4Max);
-    return btn4 = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled),
-    document.getElementById("label").textContent = btn4,
+document.getElementById("btn4").onclick = function(){
+    btn4 = Math.floor(Math.random() * (btn4Max - btn4Min + 1)) + btn4Min;
+    document.getElementById("label").textContent = btn4
     console.log('you tried randomizing 76 - 100')
 }
 document.getElementById("reset").onclick = function(){
